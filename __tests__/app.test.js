fea7fad934f89e03f82d98e3f0ec41e4fe493ef2
Katrina Cloyd <Katrina.cloyd@yahoo.com>
2021-03-30 21:33:21 -0700

    no clue what I am doing here...

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -7,4 +7,11 @@ describe('ct-lab06 routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
+
+  it('gets HTML for Index.html', async () => {
+    const res = await request(app)
+      .get('/index.html');
+    expect(res.text).toEqual(`<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><title>Index</title></head><body><h1>You have reached the Index page! </h1></body></html>`);
+  });
+
 });
